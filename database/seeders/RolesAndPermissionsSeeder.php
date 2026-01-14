4dc6466fcc3d0bb39684cc997c6985ed003334db
Helios360 <elias.treguier@gmail.com>
2026-01-14 17:16:51 +0100

    act 8 bis

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -11,16 +11,13 @@ use App\Models\User;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
         $viewDashboard = Permission::firstOrCreate(['name' => 'view dashboard']);
         $admin = Role::firstOrCreate(['name' => 'admin']);
         $admin->givePermissionTo($viewDashboard);
-        $user = User::where('email', 'hello@exemple.com')->first();
+        $user = User::where('email', 'admin@example.com')->first();
         if ($user) { $user-> assignRole('admin'); } 
     }
 }
