f4ef9b450ca6b373d2f94572a9a93485e3e51191
Helios360 <elias.treguier@gmail.com>
2026-01-16 10:03:34 +0100

    act 9 bis 2

--- app/Http/Controllers/GiftController.php
+++ app/Http/Controllers/GiftController.php
@@ -31,7 +31,7 @@ class GiftController extends Controller
     {
         $validate = $request->validate([
             'name' => 'required|string|min:3|max:50',
-            'url' => ['nullable', 'url', 'regex:/^https?:\/\/.+/i'],
+            'url' => ['nullable', 'url:http,https'],
             'details' => 'nullable|string',
             'price' => ['required', 'numeric', 'decimal:0,2'],
         ]);
@@ -62,7 +62,7 @@ class GiftController extends Controller
     {
         $validate = $request->validate([
             'name' => 'required|string|min:3|max:50',
-            'url' => ['nullable', 'url', 'regex:/^https?:\/\/.+/i'],
+            'url' => ['nullable', 'url:http,https'],
             'details' => 'nullable|string',
             'price' => ['required', 'numeric', 'decimal:0,2'],
         ]);
